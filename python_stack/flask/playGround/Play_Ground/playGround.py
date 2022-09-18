494510a8894ec29f42598184c217bbb5b8d4b103
Dana Alhaji <dana.alhaji95@gmail.com>
2022-09-18 19:51:10 +0300

    pyhton updated and html

--- python_stack/flask/playGround/Play_Ground/playGround.py
+++ python_stack/flask/playGround/Play_Ground/playGround.py
@@ -3,12 +3,18 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
 def hello():
-    return render_template("index.html",times=1)
+    return render_template("index.html",times=1, divColor= 'deeppink')
 
 @app.route("/play/<number>")
 def div(number):
     repeat = int(number)
-    return render_template("index.html", times = repeat)
+    return render_template("index.html", times = repeat ,divColor= 'deeppink')
+
+@app.route("/play/<number>/<color>")
+def divColorAndNum(number,color):
+    repeat = int(number)
+    divColor = color
+    return render_template("index.html", times = repeat, colorOfDiv =divColor )
 
 if __name__ == "__main__":
     app.run()
