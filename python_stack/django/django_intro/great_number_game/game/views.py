8320dd5d2ff386fcb3ebeb885cd2a8ef7cb9dc96
Dana Alhaji <dana.alhaji95@gmail.com>
2022-09-22 13:30:05 +0300

    update pyhton file and html

--- python_stack/django/django_intro/great_number_game/game/views.py
+++ python_stack/django/django_intro/great_number_game/game/views.py
@@ -5,12 +5,15 @@ import random
 def root(request):
     if 'randomNum' not in request.session:
         request.session['randomNum'] = random.randint(0,100)
+        print(request.session['randomNum'])
         request.session['status']=''
     return render(request ,"gameNum.html")
 def answerGuess(request):
+    request.session['status']='Result'
     if request.session['randomNum']  == int(request.POST['numberGuess']):
-        request.session['status']='true'
+        request.session['status']='You Win!!!'
+    elif request.session['randomNum']  > int(request.POST['numberGuess']):
+        request.session['status']='Too Low'
     else:
-        request.session['status']='false'
+        request.session['status']='Too High'
     return redirect('/')
-
