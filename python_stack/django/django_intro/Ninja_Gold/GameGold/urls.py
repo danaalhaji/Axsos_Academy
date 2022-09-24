f5a92bce3e962aea5d28dc6edb3f03be79e4412f
Dana Alhaji <dana.alhaji95@gmail.com>
2022-09-24 23:24:40 +0300

    update all files

--- python_stack/django/django_intro/Ninja_Gold/GameGold/urls.py
+++ python_stack/django/django_intro/Ninja_Gold/GameGold/urls.py
@@ -3,7 +3,4 @@ from . import views
 urlpatterns = [
     path('', views.root),
     path('collectfirstGold', views.firstGold),
-    # path('collectSecGold', views.SecondGold),
-    # path('collect3rdGold', views.ThirdGold),
-    # path('collect4thGold', views.forthGold),
 ]
--- python_stack/django/django_intro/Ninja_Gold/GameGold/views.py
+++ python_stack/django/django_intro/Ninja_Gold/GameGold/views.py
@@ -1,38 +1,51 @@
 from django.shortcuts import render,HttpResponse, HttpResponseRedirect, redirect
-from datetime import date, datetime
+from time import gmtime, strftime, time
 import random 
 def root(request):
     request.session.set_expiry(3600)
     if 'totalGold' not in request.session:
         request.session['totalGold']="0"
     return render(request,"index.html")
+    '''Collecting gold method'''
 def firstGold(request):
+    time =strftime(" %d-%b-%Y %H:%M %p", gmtime())
+    activity = []
     if request.method == 'POST':
         if 'Find1Gold' in request.POST:
             request.session['firstButton']= random.randint(10,20)
             request.session['totalGold'] = int(request.session['totalGold'])+int(request.session['firstButton'])
-            request.session['time']=f"You've earned, {request.session['firstButton']}"
+            newActv=f"You've entered a from and earned, {request.session['firstButton']} gold, {time}"
+            activity.append(newActv)
+            request.session['activity']=activity
             return redirect("/")
             # check the second button raise 10-20
         elif 'Find2Gold' in request.POST:
             request.session['second']= random.randint(10,20)
             request.session['totalGold'] = int(request.session['totalGold'])+int(request.session['second'])
-            request.session['time']=f"You've earned, {request.session['second']}"
+            newActv=f"You've entered a cave and earned, {request.session['second']}gold, {time}"
+            activity.append(newActv)
+            request.session['activity']=activity
             return redirect("/")
             # check the third button raise 10-20
         elif 'Find3Gold' in request.POST:
             request.session['third']= random.randint(10,20)
             request.session['totalGold'] = int(request.session['totalGold'])+int(request.session['third'])
-            request.session['time']=f"You've earned, {request.session['third']}"
+            newActv=f"You've entered a house and earned, {request.session['third']} gold, {time}"
+            activity.append(newActv)
+            request.session['activity']=activity
             return redirect("/")
             # check the 4th button raise or decrease -50-50
         elif 'Find4Gold' in request.POST:
             request.session['forth']= random.randint(-50,50)
             request.session['totalGold'] = int(request.session['totalGold'])+int(request.session['forth'])
             if request.session['forth'] >0:
-                request.session['time']=f"You've earned, {request.session['forth']}"
+                newActv=f"You completed a quest and earned , {request.session['forth']} gold, {time}"
+                activity.append(newActv)
+                request.session['activity']=activity
             else:
-                request.session['time']=f"You've LOST, {request.session['forth']}"
+                newActv=f"You failed a quest and lost, {request.session['forth']} gold,Ouch {time}"
+                activity.append(newActv)
+                request.session['activity']=activity
             return redirect("/")
 
 
