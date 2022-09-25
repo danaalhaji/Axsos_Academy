3cfaebba90ad4cda886cac97df0ccc657fd9d0dc
Dana Alhaji <dana.alhaji95@gmail.com>
2022-09-25 10:56:41 +0300

    add python and models and html

--- python_stack/django/django_orm/single_model_orm/users_app/views.py
+++ python_stack/django/django_orm/single_model_orm/users_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from multiprocessing import context
+from django.shortcuts import render, HttpResponse, redirect
+from .models import User
+def root(request):
+    context = {
+        "Users": User.objects.all()
+    }
+    return render(request,"userForm.html",context)
+def addUser(request):
+    FirstName = request.POST['firstname']
+    lastName = request.POST['lastname']
+    Email = request.POST['Email']
+    Age = request.POST['age']
+    user = User(first_name = FirstName, Last_name=lastName, email = Email,age=Age )
+    user.save()
+    return redirect("/")
 # Create your views here.
+
