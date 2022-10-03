1015e27c7af10d421da64bc6be59bcdcc228183b
Dana Alhaji <dana.alhaji95@gmail.com>
2022-10-03 14:34:44 +0300

    add python and html files

---
+++ python_stack/django/django_orm/LOGINandRegister/LoginAndRegisterAPP/urls.py
@@ -0,0 +1,12 @@
+from xml.etree.ElementInclude import include
+
+
+from django.urls import path
+from . import views
+urlpatterns =[
+    path('', views.homePage),
+    path('login',views.Login),
+    path('signup', views.SignUp),
+    path('WelcomNewUser', views.signUpSucess),
+    path('welcomUser', views.loginSuccess),
+]
