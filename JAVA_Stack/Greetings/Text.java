a7078bee60ec365dd651e3dff66e2a003521bf01
Dana Alhaji <dana.alhaji95@gmail.com>
2022-10-24 11:16:00 +0300

    Write first Java Program

---
+++ JAVA_Stack/Greetings/Text.java
@@ -0,0 +1,6 @@
+public class Text {
+	public static void main(String[] args) {
+        String newline = System.lineSeparator();
+		System.out.println("My Name is Dana" + newline + "I'm 28 years old" + newline + "My hometown is Plaestine, Nablus");
+}
+}
