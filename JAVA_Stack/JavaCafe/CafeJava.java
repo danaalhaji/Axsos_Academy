152c154c533f9accf3cebab73db686fada35c7d9
Dana Alhaji <dana.alhaji95@gmail.com>
2022-10-24 11:46:53 +0300

    add more details to java cafe

---
+++ JAVA_Stack/JavaCafe/CafeJava.java
@@ -0,0 +1,36 @@
+public class CafeJava {
+    public static void main(String[] args) {
+        // APP VARIABLES
+        // Lines of text that will appear in the app. 
+        String generalGreeting = "Welcome to Cafe Java, ";
+        String pendingMessage = ", your order will be ready shortly";
+        String readyMessage = ", your order is ready";
+        String displayTotalMessage = "Your total is ";
+        
+        // Menu variables (add yours below)
+        double mochaPrice = 3.5;
+        double dripCoffee = 2.5;
+        double cafeLatte = 4.5;
+        double cappuccino = 3.8;
+    
+        // Customer name variables (add yours below)
+        String customer1 = "Cindhuri";
+        String customer2 = "Sam";
+        String customer3 = "Jimmy";
+        String customer4 = "Noah";
+    
+        // Order completions (add yours below)
+        boolean isReadyOrder1 = false;
+        boolean isReadyOrder2 = true;
+        boolean isReadyOrder3 = false;
+        boolean isReadyOrde4r = true;
+
+    
+        // APP INTERACTION SIMULATION (Add your code for the challenges below)
+        // Example:
+        System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
+        System.out.println(customer1 + pendingMessage );
+        System.out.println(generalGreeting + customer2 + readyMessage + "You Ordered CafeLatte and," +displayTotalMessage + cafeLatte +"$" );
+    	// ** Your customer interaction print statements will go here ** //
+    }
+}
