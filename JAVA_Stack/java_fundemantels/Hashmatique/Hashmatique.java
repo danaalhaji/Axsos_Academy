7d1b92721ef0002c48cc09cbe2338e155f35ea5c
Dana Alhaji <dana.alhaji95@gmail.com>
2022-10-25 20:24:37 +0300

    update the java project for songs and singer

--- JAVA_Stack/java_fundemantels/Hashmatique/Hashmatique.java
+++ JAVA_Stack/java_fundemantels/Hashmatique/Hashmatique.java
@@ -1,5 +1,16 @@
+import java.util.HashMap;
+import java.util.Set;
 public class Hashmatique{
     public static void main(String[] args){
-
+        HashMap<String , String> trackList = new HashMap<String , String>();
+        trackList.put("Unholy" , "Sam Smith");
+        trackList.put("Sebooh" , " Amr Mostafa");
+        trackList.put("CalmDown" , "Selena Gomez");
+        trackList.put("BellaBella" , " Naika");
+        System.out.println(trackList.get("Unholy"));
+        Set<String> titles = trackList.keySet();
+        for(String title : titles ){
+            System.out.println(title + " - " + trackList.get(title));
+        }
     }
 }
