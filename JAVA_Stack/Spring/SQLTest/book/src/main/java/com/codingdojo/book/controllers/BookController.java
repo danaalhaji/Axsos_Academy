a52c16d7310f4fe08abcf1efce7e457da900a75a
Dana Alhaji <dana.alhaji95@gmail.com>
2022-11-05 18:00:40 +0200

    add a show all books route and show one book from the table

--- JAVA_Stack/Spring/SQLTest/book/src/main/java/com/codingdojo/book/controllers/BookController.java
+++ JAVA_Stack/Spring/SQLTest/book/src/main/java/com/codingdojo/book/controllers/BookController.java
@@ -1,10 +1,13 @@
 package com.codingdojo.book.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.codingdojo.book.models.Book;
 import com.codingdojo.book.services.BookService;
@@ -12,12 +15,28 @@ import com.codingdojo.book.services.BookService;
 @Controller
 public class BookController {
 	@Autowired
-	BookService bookServices;
-@GetMapping("/book/{bookId}")
-public String bookView(@PathVariable("bookId") long bookId, Model model) {
-	Book book = bookServices.findBook(bookId);
-	model.addAttribute(book);
-	return"index.jsp";
+    private BookService bookService;
+    
+    public void BooksController(BookService bookService) {
+        this.bookService = bookService;
+    }
+    
+//    Method to show all books
+
+    @RequestMapping("/books")
+    public String index(Model model) {
+        List<Book> books = bookService.allBooks();
+        model.addAttribute("books", books);
+        return "/books/index.jsp";
+    }
+    
+// Method to show one book by ID
+    
+    @GetMapping("/book/{bookId}")
+    public String bookView(@PathVariable("bookId") long bookId, Model model) {
+    	Book book = this.bookService.findBook(bookId);
+    	model.addAttribute(book);
+    	return"index.jsp";
 	
 }
 
