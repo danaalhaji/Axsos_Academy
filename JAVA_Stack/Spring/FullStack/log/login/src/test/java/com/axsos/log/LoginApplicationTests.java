971e9ad2f4cbe92f6b452ff294506cc72b0153fb
Dana Alhaji <dana.alhaji95@gmail.com>
2022-11-09 13:13:36 +0200

    do login and reg

---
+++ JAVA_Stack/Spring/FullStack/log/login/src/test/java/com/axsos/log/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axsos.log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
