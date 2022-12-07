b4668048da7f10a296ea0bf3294c78caa899038d
Dana Alhaji <dana.alhaji95@gmail.com>
2022-12-07 14:29:35 +0200

    first react application

---
+++ MERN/React/firstJsx/firstreactjsx/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Dojo</h1>
+      <h2>Things i ndeed to do:</h2>
+      <ul>
+        <li>
+            Learn React
+        </li>
+        <li>
+            Climb Mt, Everest
+        </li>
+        <li>
+            Run a marothon
+        </li>
+        <li>
+            Feed the dogs
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
