6ac53e09a64dd3e0a73b9597fe5d6f8932e8ca18
Dana Alhaji <dana.alhaji95@gmail.com>
2022-12-11 21:10:32 +0200

    add delete button for each created div

--- MERN/React/boxgenerator/src/components/ColorDisplay.jsx
+++ MERN/React/boxgenerator/src/components/ColorDisplay.jsx
@@ -1,16 +1,18 @@
-import React ,{ Component } from 'react'
+import React ,{ Component }  from 'react';
+
 
 const ColorDisplay = (props) => {
-
+  const removeNode = (idx) => document.getElementById(`id-${idx}`).remove();
   return (
     <div style={{display :"flex"}}>
       {props.newColorBox?.map((item ,idx)=>{
-        return (<div style={ {width :"150px",
+        return ( 
+        <div style={ {width :"150px",
         height : "150px",
         padding: "5px",
         border: "1px solid red",
         margin :"0 auto",
-        backgroundColor:item}} ></div>);
+        backgroundColor:item}} key={idx} id={`id-${idx}`} > <button onClick={() => removeNode(idx)}>x</button></div>);
       })}
       {/* <div style={{width:`${props.boxs.size}px` ,height:`${props.boxs.size}px`, backgroundColor:props.boxs.color}}>Hi</div> */}
       {/* {props.boxs?.map((item ,idx)=>{
