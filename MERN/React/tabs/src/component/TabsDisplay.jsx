42312e23f87ac37c17a590bc9669aef3429bad19
Dana Alhaji <dana.alhaji95@gmail.com>
2022-12-11 23:11:16 +0200

    add motions to second tab

--- MERN/React/tabs/src/component/TabsDisplay.jsx
+++ MERN/React/tabs/src/component/TabsDisplay.jsx
@@ -2,10 +2,22 @@ import React  from 'react'
 // import ReactTransitionGroup from 'react-addons-transition-group' // ES6
 // var ReactTransitionGroup = require('react-addons-transition-group') 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { motion } from "framer-motion";
 const TabsDisplay = (props) => {
    return (
     <div>
-        <p>{props.newChange}</p>
+        <motion.p
+            animate={{ x: [50, 150, 50], opacity: 1, scale: 1 }}
+            transition={{
+                duration: 10,
+                delay: 0.6,
+                ease: [0.8, 1, 1.5, 1.5],
+            }}
+            initial={{ opacity: 0, scale: 0.6 }}
+            whileHover={{ scale: 3 }}
+            >
+        <p style={{color:"red"}}>{props.newChange}</p>
+        </motion.p>
     </div>
   )
 }
--- MERN/React/tabs/src/component/TabsForm.jsx
+++ MERN/React/tabs/src/component/TabsForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
-
+import { motion } from "framer-motion";
 const TabsForm = (props) => {
     const dict =[{label:"Tab 1", content :"This is Tab 1"}, {label:"Tab 2", content :"This is Tab 2"}, 
     {label:"Tab 3", content :"This is Tab 3"}];
@@ -13,10 +13,30 @@ const TabsForm = (props) => {
         props.changeTab(tab);
         console.log()
     }
+
   return (
+    // style =()=> {{
+    //     width: "100px",
+    //     height: "100px",
+    //     backgroundColor: "red",
+    //     position: "relative";
+    //     animation: name(),
+    //     animation-duration: "4s",
+    // }}
+    //   @function name(div) {
+    //     keyframes
+    //   } example {
+    //     0%   {backgroundColor:"red", left:"0px", top:"0px"}
+    //     25%  {backgroundColor:"yellow", left:"200px", top:"0px"}
+    //     50%  {backgroundColor:"blue", left:"200px", top:"200px"}
+    //     75%  {backgroundColor:"green", left:"0px", top:"200px"}
+    //     100% {backgroundColor:"red", left:"0px", top:"0px"}
+    //   }
     <div>
     <ButtonGroup aria-label="Basic example">
-      {dict.map((item, index) => <Button variant="warning" key={ index } onClick={(e) => changeToNewTab(e, item.content)}>{item.label}</Button>)}
+      {dict.map((item, index) =>
+      <Button variant="warning" key={ index } onClick={(e) => changeToNewTab(e, item.content)}>
+      {item.label}</Button>  )}
     </ButtonGroup>
     </div>
   )
